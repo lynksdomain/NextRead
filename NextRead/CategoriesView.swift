2b3f07f4a0e830649205cc46d746e0ceff4677d3
Lynk <lynk@Lynks-MacBook-Pro.local>
2019-01-28 16:29:02 -0500

    first commit

---
+++ NextRead/CategoriesView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesView.swift
+//  NextRead
+//
+//  Created by Lynk on 1/28/19.
+//  Copyright © 2019 Lynk. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NextRead/CategoriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesViewController.swift
+//  NextRead
+//
+//  Created by Lynk on 1/28/19.
+//  Copyright © 2019 Lynk. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
